f702349012c66782c08b7949aa929aed1ac68d0f
Manuel Torrez <mastervado98@gmail.com>
2016-11-19 15:42:36 -0600

    Prueba splash screen

---
+++ Demo/app/src/main/java/com/gangofseven/labs/app/demo/EasySplashScreenActivity.java
@@ -0,0 +1,31 @@
+package com.gangofseven.labs.app.demo;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class EasySplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View easySplashScreenView = new EasySplashScreen(this)
+                .withFullScreen()
+                .withTargetActivity(MainActivity.class)
+                .withSplashTimeOut(1000)
+                .withBackgroundResource(android.R.color.holo_red_light)
+                .withHeaderText("Header")
+                .withFooterText("Copyright 2016")
+                .withBeforeLogoText("My cool company")
+                .withLogo(R.mipmap.ic_chancho)
+                .withAfterLogoText("Some more details")
+                .create();
+
+        setContentView(easySplashScreenView);
+/*        setContentView(R.layout.activity_easy_splash_screen);*/
+    }
+}
